51ed10492109657a6993ba66ce6ba1f58d0419df
Neiman22 <pushkarev_av@inbox.ru>
2023-08-02 15:20:41 +0300

    fix: add shutdown

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+import { stdin, stdout } from 'process';
+
 const args = process.argv.slice(2);
-const userName = String(args).split('=')[1];
-console.log(`Welcome to the File Manager, ${userName}!`);
+const USER_NAME = String(args).split('=')[1];
+
+const currentDir = () => {
+  const cwd = process.cwd();
+  stdout.write(`You are currently in ${cwd}\n`);
+}
+
+const workFinished = () => {
+  stdout.write(`Thank you for using File Manager, ${USER_NAME}, goodbye!\n`);
+  process.exit(0);
+}
+
+const commandHandler = (command) => {
+  switch (command.trim()) {
+    case '.exit':
+        workFinished();
+        break;
+    default: stdout.write('Unknown Command, please enter correct command or .exit: ');
+ }
+}
+
+const app = () => {
+  console.log(`Welcome to the File Manager, ${USER_NAME}!`);
+  currentDir();
+
+  stdout.write(`Please enter your command or .exit: `)
+  stdin.on('data', data => {
+    commandHandler(data.toString());
+  });
+
+  process.on('SIGINT', () => {
+    stdout.write('\n');
+    workFinished();
+  });
+}  
+app();
